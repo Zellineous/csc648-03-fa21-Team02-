f5dc634a95e81290b1d76e0a1820b9bf71015452
the-96 <godcantpee@gm>
2021-09-15 18:56:53 -0700

    added templates folder for .html files

--- application/application.py
+++ application/application.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
-
 application = Flask(__name__)
 application.config['SECRET_KEY'] = '123456789'
 
 
 @application.route('/')
 def home():
-    return '<h2>Hello from team 2,</h2> This is the home page. and it works with html, update from my own IDE'
+    return render_template('home.html')
+@application.route('/about')
+def about():
+    return '<h2>this is the about PAGE,</h2> This is the home page. and it works with html, update from my own IDE'
+
 
 
 if __name__ == '__main__':
