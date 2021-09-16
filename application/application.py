507bfe7e722e302b3f08d497fc721e19b3f1b784
the-96 <godcantpee@gm>
2021-09-16 16:14:12 -0700

    added layout.html, will add instructions to discord

--- application/application.py
+++ application/application.py
@@ -9,8 +9,9 @@ def home():
 @application.route('/about')
 def about():
     return '<h2>this is the about PAGE,</h2> This is the home page. and it works with html, update from my own IDE'
-
-
+@application.route('/thomas')
+def thomas():
+    return render_template('thomas.html')
 
 if __name__ == '__main__':
     application.run(debug=True)
