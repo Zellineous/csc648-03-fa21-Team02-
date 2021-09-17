bc8fb5e7b1ad3549917117a17f9ecd70862235e6
the-96 <godcantpee@gm>
2021-09-17 10:47:55 -0700

    urls work, layout used for each .html

--- application/application.py
+++ application/application.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template
+from string import Template
+
 application = Flask(__name__)
 application.config['SECRET_KEY'] = '123456789'
 
-
+#in .html files, make sure to href= to these routes, not the location of the .html files themselves
 @application.route('/')
+@application.route('/home')
 def home():
     return render_template('home.html')
 @application.route('/about')
 def about():
-    return '<h2>this is the about PAGE,</h2> This is the home page. and it works with html, update from my own IDE'
+    return render_template('about.html')
 @application.route('/thomas')
 def thomas():
     return render_template('thomas.html')
+
+@application.route('/team/<member>_about')
+def team_member_about(member):
+    return render_template('team/' + member + '.html')
+
 
 if __name__ == '__main__':
     application.run(debug=True)
