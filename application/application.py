3b4d1670c7b8670a7b051addac35ff11fa205b63
the-96 <godcantpee@gm>
2021-11-03 17:47:28 -0700

    search is working, nothing to search though, and the search bar looks weird...

--- application/application.py
+++ application/application.py
@@ -1,27 +1,36 @@
-from flask import Flask, render_template
-from string import Template
+from flask import Flask, render_template, request, redirect, url_for
 
 application = Flask(__name__)
 application.config['SECRET_KEY'] = '123456789'
 
-#in .html files, make sure to href= to these routes, not the location of the .html files themselves
-@application.route('/')
 
-@application.route('/home')
-def home():
+# in .html files, make sure to href= to these routes, not the location of the .html files themselves
+@application.route('/', methods=['GET','POST'])
+def index():
+    print(request.method)
+    if request.method == 'POST':
+        print(request.form)
+        search = request.form.get('search')
+        return redirect(url_for('results', search=search))
+
     return render_template('home.html')
+
 
 @application.route('/about')
 def about():
     return render_template('about.html')
 
+
 @application.route('/browse')
 def browse():
     return render_template('browse.html')
 
-@application.route('/thomas')
-def thomas():
-    return render_template('thomas.html')
+
+@application.route('/results', methods=['GET', 'POST'])
+def results():
+    search = request.args.get('search',None)
+    return render_template('results.html', search=search)
+
 
 @application.route('/team/<member>_about')
 def team_member_about(member):
